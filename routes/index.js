6b1db8af590605eff909186b2a96c87d32c185c9
Spyros Karidis <spyrology@gmail.com>
2015-02-25 20:56:13 -0500

    Add authentication to admin route

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router
 	.get('/payment-form', function(req, res) {
 		res.render('payment-form', { stylesheet: 'payment-form' });
 	})
-	.get('/admin', function(req, res) {
+	.get('/admin', isLoggedIn, function(req, res) {
 		res.render('admin', { stylesheet: 'admin' });
 	})
 	.post('/admin', opportunities.create)
@@ -46,5 +46,28 @@ router
   }));
 
   console.log('here');
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/log-in');
+}
+
+var requiresAdmin = function() {
+  return [
+    isLoggedIn('/admin'),
+    function(req, res, next) {
+      if (req.user && req.user.isAdmin === true)
+        next();
+      else
+        res.send(401, 'Unauthorized');
+    }
+  ]
+};
 
 module.exports = router;
