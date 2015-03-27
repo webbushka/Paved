e73f974c51650a8fe353767f30acabb136d98dbb
AJ Webb <webbushka@gmail.com>
2015-03-26 20:05:52 -0600

    added the opportunity stuff

--- routes/index.js
+++ routes/index.js
@@ -7,80 +7,101 @@ var router = express.Router();
 /* GET home page. */
 
 router
-	/*
+/*
 	.get('/', function(req, res) {
   	res.render('landing-page.hbs', { layout: false });
 	})
 	.post('/', emails.create);
 	*/
-	.get('/', function (req, res) {
-		res.render('index', { stylesheet: 'index' });
-	})
-	.get('/opportunities', function (req, res) {
-		companies.find(req, function (err, opps) {
-			// if (req.get('content-type') === 'application/json') {
-			// 	return res.json(opps);
-			// }
-			if(err) return console.log(err);
-			res.render('opportunities', {stylesheet: 'opportunities', companies: opps})
-		});
-	})
-	.get('/focused', function (req, res) {
-		res.render('focused', { stylesheet: 'focused' });
-	})
-	.get('/evaluation/:id', companies.show)
-	.get('/payment-form', function (req, res) {
-		res.render('payment-form', { stylesheet: 'payment-form' });
-	})
-	.get('/admin', isLoggedIn, function (req, res) {
-		companies.find(req, function (err, opps) {
-			if(err) return console.log(err);
-			res.render('admin', { stylesheet: 'admin', companies: opps });
-		});
-	})
-	.post('/admin', companies.create)
-	.put('/admin', companies.update)
-	.get('/sign-up', function (req, res) {
-		res.render('sign-up', {layout: false});
-	})
-	// process the signup form
-  .post('/sign-up', passport.authenticate('local-signup', {
-      successRedirect : '/', // redirect to the secure profile section
-      failureRedirect : '/sign-up', // redirect back to the signup page if there is an error
-      failureFlash : true // allow flash messages
-  }))
-  .get('/log-in', function (req, res) {
-		res.render('log-in', {layout: false});
-	})
-  .post('/log-in', passport.authenticate('local-login', {
-      successRedirect : '/', // redirect to the secure profile section
-      failureRedirect : '/log-in', // redirect back to the signup page if there is an error
-      failureFlash : true // allow flash messages
-  }));
+   .get('/', function(req, res) {
+      res.render('index', {
+         stylesheet: 'index'
+      });
+   })
+   .get('/opportunities', function(req, res) {
+      companies.find(req, function(err, opps) {
+         // if (req.get('content-type') === 'application/json') {
+         // 	return res.json(opps);
+         // }
+         if (err) return console.log(err);
+         opps.forEach(function(company) {
+            company.opportunities.forEach(function(opp, i, list) {
+               list[i].companyId = company._id;
+            });
+         });
+         res.render('opportunities', {
+            stylesheet: 'opportunities',
+            companies: opps
+         });
+      });
+   })
+   .get('/opportunities/:companyId/evaluation/:id', companies.show)
+   .get('/focused', function(req, res) {
+      res.render('focused', {
+         stylesheet: 'focused'
+      });
+   })
+   .get('/payment-form', function(req, res) {
+      res.render('payment-form', {
+         stylesheet: 'payment-form'
+      });
+   })
+   .get('/admin', isLoggedIn, function(req, res) {
+      companies.find(req, function(err, opps) {
+         if (err) return console.log(err);
+         res.render('admin', {
+            stylesheet: 'admin',
+            companies: opps
+         });
+      });
+   })
+   .post('/admin', companies.create)
+   .put('/admin', companies.update)
+   .get('/sign-up', function(req, res) {
+      res.render('sign-up', {
+         layout: false
+      });
+   })
+   // process the signup form
+   .post('/sign-up', passport.authenticate('local-signup', {
+      successRedirect: '/', // redirect to the secure profile section
+      failureRedirect: '/sign-up', // redirect back to the signup page if there is an error
+      failureFlash: true // allow flash messages
+   }))
+   .get('/log-in', function(req, res) {
+      res.render('log-in', {
+         layout: false
+      });
+   })
+   .post('/log-in', passport.authenticate('local-login', {
+      successRedirect: '/', // redirect to the secure profile section
+      failureRedirect: '/log-in', // redirect back to the signup page if there is an error
+      failureFlash: true // allow flash messages
+   }));
 
-  console.log('here');
+console.log('here');
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
 
-    // if user is authenticated in the session, carry on 
-    if (req.isAuthenticated())
-        return next();
+   // if user is authenticated in the session, carry on
+   if (req.isAuthenticated())
+      return next();
 
-    // if they aren't redirect them to the home page
-    res.redirect('/log-in');
+   // if they aren't redirect them to the home page
+   res.redirect('/log-in');
 }
 
 var requiresAdmin = function() {
-  return [
-    isLoggedIn('/admin'),
-    function (req, res, next) {
-      if (req.user && req.user.isAdmin === true)
-        next();
-      else
-        res.send(401, 'Unauthorized');
-    }
-  ]
+   return [
+      isLoggedIn('/admin'),
+      function(req, res, next) {
+         if (req.user && req.user.isAdmin === true)
+            next();
+         else
+            res.send(401, 'Unauthorized');
+      }
+   ]
 };
 
 module.exports = router;
--- routes/opportunities.js
+++ routes/opportunities.js
@@ -1,83 +1,96 @@
 var express = require('express');
 var mongoose = require('mongoose');
 
+var oppSchema = mongoose.Schema({
+   position: {
+      type: String,
+      required: true
+   },
+   status: {
+      type: String,
+      required: true
+   },
+   requirements: {
+      type: String,
+      required: true
+   },
+   timeestimate: {
+      type: String,
+      required: true
+   },
+   price: {
+      type: Number,
+      required: true
+   },
+   description: {
+      type: String,
+      required: true
+   }
+});
+
 // define the schema for our opportunity model
 var companySchema = mongoose.Schema({
-		name					: String,
-		opportunities	: [
-			{
-				position			: {type: String, required: true},
-				status				: {type: String, required: true},
-  			requirements  : {type: String, required: true},
-  			timeestimate	: {type: String, required: true},
-  			price					: {type: Number, required: true},
-  			description		: {type: String, required: true}  
-			}
-		]
+   name: String,
+   opportunities: [oppSchema]
 });
 
 var Company = mongoose.model('Company', companySchema);
 
-var getOpportunity = function (req) {
-	var opp = {
-		position: req.body.position, 
-		status: req.body.status, 
-		requirements: req.body.requirements, 
-		timeestimate: req.body.timeestimate, 
-		price: req.body.price,
-		description: req.body.description
-	};
-	console.log('opp', opp);
-	return opp;
+var getOpportunity = function(req) {
+   var opp = {
+      position: req.body.position,
+      status: req.body.status,
+      requirements: req.body.requirements,
+      timeestimate: req.body.timeestimate,
+      price: req.body.price,
+      description: req.body.description
+   };
+   return opp;
 };
 
-var getEvaluation = function (req) {
-	var eval = {
-		name: req.body.name,
-		position: req.body.position,
-		timeestimate: req.body.timeestimate,
-		description: req.body.description
-	};
-	return eval;
-	console.log(eval);
+exports.create = function(req, res) {
+   var company = new Company({
+      name: req.body.name,
+      opportunities: [getOpportunity(req)]
+   });
+   company.save(function(err, item) {
+      if (err) return console.error(err);
+      res.render('opportunities', {
+         stylesheet: 'opportunities'
+      });
+   });
 };
 
-exports.create = function (req, res) {
-	var company = new Company({
-		name: req.body.name,
-		opportunities: [getOpportunity(req)]
-	});
-	console.log(company);
-	company.save(function (err, item) {
-  	if (err) return console.error(err);
-  	res.render('opportunities', {stylesheet: 'opportunities'});
-  });
+exports.update = function(req, res) {
+   Company.findById(req.body.company, function(err, company) {
+      if (err) return console.log(err);
+      company.opportunities.create(getOpportunity(req));
+      company.save(function(err) {
+         if (err) return console.log(err);
+         res.json(company);
+      });
+   });
 };
 
-exports.update = function (req, res) {
-	Company.findById(req.body.company, function(err, company) {
-		if(err) return console.log(err);
-		company.opportunities.push(getOpportunity(req));
-		company.save(function(err) {
-			if(err) return console.log(err);
-			res.json(company);
-		})
-	});
+exports.find = function(req, done) {
+   Company.find(function(err, opps) {
+      if (typeof done === 'function') {
+         done(err, opps);
+      } else {
+         throw ('Expects a function as a second argument');
+      }
+   });
 };
 
-exports.find = function (req, done) {
-	Company.find(function (err, opps) {
-		if(typeof done === 'function') {
-			done(err, opps);
-		}
-		else {
-			throw('Expects a function as a second argument');
-		}
-	});
-};
-
-exports.show = function (req, res) {
-	var eval = Company.findById(Company, {id: req.body.id});
-	console.log(eval);
-	res.render('evaluation', {stylesheet: 'evaluation', Company: eval});
+exports.show = function(req, res) {
+   Company.findById(req.params.companyId, function(err,
+      company) {
+      if (err) throw err;
+      var evaluation = company.opportunities.id(req.params.id);
+      res.render('evaluation', {
+         stylesheet: 'evaluation',
+         company: company,
+         opportunity: evaluation
+      });
+   });
 };
